bb81a2d81eb0489825aa798fe086023bf95fe31e
Jay Bednar <ironviralmedia@gmail.com>
2018-04-03 17:57:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,19 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
-    erb :newteam 
+    erb :newteam
   end
 
-  post '/team' do 
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     
-  end 
+    
+  end
 
 end
